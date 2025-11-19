5799849a05c5cd0d22f582c21757b9119b27959f
boatman3132 <boatman3132@gmail.com>
2025-11-19 15:33:24 +0800

    取消google analytics

--- js/gtag-config.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-        window.dataLayer.push(arguments);
-    }
-    window.gtag = window.gtag || gtag;
-    gtag('js', new Date());
-    gtag('config', 'G-XQFD3ZFHJ4');
-})();
